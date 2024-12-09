ede4ea4e0693a99bca1239ba39038c5e29c2a65f
shaluydv01 <ydvshalu420@gmail.com>
2024-12-09 13:45:56 +0530

    code-blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Gsap from "./Pages/GSAP/Gsap";
+import ReactPage from "./Pages/ReactPage/ReactPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReactCode from "./PracticeCode/ReactCode/ReactCode";
 
 const App = () => {
   return (
-    <>
-      <Gsap />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ReactPage />}></Route>
+        <Route path="/gsap" element={<Gsap />}></Route>
+        <Route path="/reactcode" element={<ReactCode />}></Route>
+      </Routes>
+    </Router>
   );
 };
 
